3f8df974638aeb14f6a7225d83562b29a6593f6a
Justine Vareille <vareille.justine@gmail.com>
2019-06-05 09:50:49 +0200

    config eslint

--- src/Signup.js
+++ src/Signup.js
@@ -1,104 +1,116 @@
 import React, { Component } from 'react';
-import withFirebaseContext from './Firebase/withFirebaseContext';
 import { Link } from 'react-router-dom';
-import { withRouter } from "react-router";
+import { withRouter } from 'react-router';
+import Button from '@material-ui/core/Button';
+import withFirebaseContext from './Firebase/withFirebaseContext';
 
 class Signup extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            user: null,
-        }
-        const { auth } = this.props
-        auth.onAuthStateChanged((user) => {
-            if (user) {
-                this.users(user)
-            }
-        });
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: null,
+    };
+    const { auth } = this.props;
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        this.users(user);
+      }
+    });
+  }
 
     login = (choice) => {
-        const { auth, googleProvider, facebookProvider } = this.props;
-        if (choice === 'google') {
-            // auth.signInWithPopup(googleProvider) //à remplacer sur une app mobile 
-            //     .then((result) => {
-            //         const user = result.user;
-            //         this.setState({
-            //             user
-            //         }, this.users(user));
-            //     });
-            auth.signInWithRedirect(googleProvider)
-                .then(() => { return auth.getRedirectResult(); })
-                .then((result) => {
-                    const user = result.user;
-                    this.setState({
-                        user
-                    });
-                    // this.props.history.push("/dashboard");
-                }).catch((error) => {
-                    const errorMessage = error.message;
-                    console.log(errorMessage)
-                });
-        }
+      const { auth, googleProvider, facebookProvider } = this.props;
+      if (choice === 'google') {
+        // auth.signInWithPopup(googleProvider) //à remplacer sur une app mobile
+        //     .then((result) => {
+        //         const user = result.user;
+        //         this.setState({
+        //             user
+        //         }, this.users(user));
+        //     });
+        auth.signInWithRedirect(googleProvider)
+          .then(() => auth.getRedirectResult())
+          .then((result) => {
+            const { user } = result;
+            this.setState({
+              user,
+            });
+            // this.props.history.push("/dashboard");
+          }).catch((error) => {
+            const errorMessage = error.message;
+            console.log(errorMessage);
+          });
+      }
 
-        if (choice === 'facebook') {
-            // auth.signInWithPopup(facebookProvider) //à remplacer sur une app mobile 
-            //     .then((result) => {
-            //         const user = result.user;
-            //         this.setState({
-            //             user
-            //         }, this.users(user));
-            //     });
-            auth.signInWithRedirect(facebookProvider)
-                .then(() => { return auth.getRedirectResult(); })
-                .then((result) => {
-                    const user = result.user;
-                    this.setState({
-                        user
-                    });
-                    // this.props.history.push("/dashboard");
-                }).catch((error) => {
-                    const errorMessage = error.message;
-                    console.log(errorMessage)
-                });
-        }
+      if (choice === 'facebook') {
+        // auth.signInWithPopup(facebookProvider) //à remplacer sur une app mobile
+        //     .then((result) => {
+        //         const user = result.user;
+        //         this.setState({
+        //             user
+        //         }, this.users(user));
+        //     });
+        auth.signInWithRedirect(facebookProvider)
+          .then(() => auth.getRedirectResult())
+          .then((result) => {
+            const { user } = result;
+            this.setState({
+              user,
+            });
+            // this.props.history.push("/dashboard");
+          }).catch((error) => {
+            const errorMessage = error.message;
+            console.log(errorMessage);
+          });
+      }
     }
 
     users = (user) => {
-
-        //Récupération du Firestore grâce à context
-        const { firestore } = this.props;
-        //Envoi d'infos dans le cloud Firestore
-        firestore.doc(`users/${user.uid}`).set({
-            name: user.displayName,
-            uid: user.uid,
-        }, { merge: true });
-        this.props.history.push("/dashboard");
+      // Récupération du Firestore grâce à context
+      const { firestore, history } = this.props;
+      // Envoi d'infos dans le cloud Firestore
+      firestore.doc(`users/${user.uid}`).set({
+        name: user.displayName,
+        uid: user.uid,
+      }, { merge: true });
+      history.push('/dashboard');
     }
 
 
     render() {
-        const { user } = this.state;
-        return (
-            <div className="signin" style={{ color: 'black' }}>
-            <img src="./assets/logo.png" alt="logo"/>
-            <h1>Elearning</h1>
-            <h3>Apprendre en s’amusant de façon ludique</h3>
-                {user
-                    ? <p>Signed in!</p>
-                    : <>
-                        <p> Please sign in</p>
-                        <button onClick={() => this.login('google')}>Sign in with Google</button>
-                        <button onClick={() => this.login('facebook')}>Sign in with Facebook</button>
-                        <Link to="/signin">
-                            <button>Sign in with Email</button>
-                        </Link>
-                        <p><Link to="/connect">Already have an account?</Link></p>
-                    </>
-                }
-            </div >
+      const { user } = this.state;
+      return (
+        <div className="signin" style={{ color: 'black' }}>
+          <img src="./assets/logo.png" alt="logo" />
+          <h1>Elearning</h1>
+          <h3>Apprendre en s’amusant de façon ludique</h3>
+          {user
+            ? <p>Signed in!</p>
+            : (
+              <>
+                <p> Please sign in</p>
 
-        );
+                <Button
+                  fullWidthsize="medium"
+                  color="primary"
+                  variant="contained"
+                  style={{ position: 'fixed', borderRadius: '20px' }}
+                  onClick={() => this.login('google')}
+                >
+                Sign in with Google
+
+                </Button>
+                <button onClick={() => this.login('facebook')}>Sign in with Facebook</button>
+                <Link to="/signin">
+                  <button>Sign in with Email</button>
+                </Link>
+                <p><Link to="/connect">Already have an account?</Link></p>
+              </>
+            )
+                }
+        </div>
+
+      );
     }
 }
 

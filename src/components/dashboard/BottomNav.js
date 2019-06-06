7559713cc59c53a3abc42db2603ae0eb0c11e031
Lau <laurent.henriques@gmail.com>
2019-06-06 11:37:04 +0200

    add BottomNav icon

--- src/components/dashboard/BottomNav.js
+++ src/components/dashboard/BottomNav.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
-import RestoreIcon from '@material-ui/icons/Restore';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import LocationOnIcon from '@material-ui/icons/LocationOn';
+import Edit from '@material-ui/icons/Edit';
+import Category from '@material-ui/icons/Category';
+import Folder from '@material-ui/icons/Folder';
+import AccountBox from '@material-ui/icons/AccountBox';
 
 const useStyles = makeStyles({
   root: {
@@ -29,10 +30,10 @@ export default function BottomNav() {
       showLabels
       className={classes.root}
     >
-      <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
-      <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
-      <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
-      <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+      <BottomNavigationAction label="Création" icon={<Edit />} />
+      <BottomNavigationAction label="Dashboard" icon={<Category />} />
+      <BottomNavigationAction label="Mes cours" icon={<Folder />} />
+      <BottomNavigationAction label="Profile" icon={<AccountBox />} />
     </BottomNavigation>
   );
 }

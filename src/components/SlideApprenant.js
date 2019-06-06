76aed4c4736ecc284e44a58ba0127411894053ef
DJCrispyRice <flyingempy@gmail.com>
2019-06-07 00:26:33 +0200

    version avec affichage slides créées

--- src/components/SlideApprenant.js
+++ src/components/SlideApprenant.js
@@ -2,43 +2,49 @@ import React, { Component } from 'react';
 import { withRouter } from 'react-router';
 import withFirebaseContext from '../Firebase/withFirebaseContext';
 import '../App.scss';
+import ReactHtmlParser from 'react-html-parser'; 
 
 class SlideApprenant extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      infoSlide: {},
-    };
-    this.getInfo();
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            infoSlide: null,
+        };
+        this.getInfo();
+    }
 
-  getInfo = () => {
-    const { firestore } = this.props;
-    const docRef = firestore.collection('parcours').doc(localStorage.getItem('id')).collection('cours').doc(localStorage.getItem('id'));
-    docRef.get().then((doc) => {
-      if (doc.exists) {
-        const infoSlide = doc.data();
-        this.setState({
-          infoSlide,
+    getInfo = () => {
+        const { firestore } = this.props;
+        const docRef = firestore.collection('parcours').doc(localStorage.getItem('id')).collection('cours').doc(localStorage.getItem('id'));
+        docRef.get().then((doc) => {
+            if (doc.exists) {
+                const infoSlide = doc.data();
+                this.setState({
+                    infoSlide,
+                });
+            } else {
+                // doc.data() will be undefined in this case
+                console.log('No such document!');
+            }
+        }).catch((error) => {
+            console.log('Error getting document:', error);
         });
-      } else {
-        // doc.data() will be undefined in this case
-        console.log('No such document!');
-      }
-    }).catch((error) => {
-      console.log('Error getting document:', error);
-    });
-  }
+    }
 
 
-  render() {
-    const { infoSlide } = this.state;
-    return (
-      <div>
-        {infoSlide.slides}
-      </div>
-    );
-  }
+    render() {
+        const { infoSlide } = this.state;
+        return (
+            <>
+                <div style={{ border: '2px solid black' }}>
+                    {infoSlide && ReactHtmlParser (infoSlide.slides[1]) }
+                </div>
+                <div style={{ border: '2px solid black' }}>
+                {infoSlide && ReactHtmlParser (infoSlide.slides[2]) }
+                </div>
+            </>
+        );
+    }
 }
 
 export default withRouter(withFirebaseContext(SlideApprenant));

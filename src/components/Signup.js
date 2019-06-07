e7ef31b47a054b2fdb0d0684e8ca71da61ccb86d
Justine Vareille <vareille.justine@gmail.com>
2019-06-07 10:37:19 +0200

    mise en page

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -58,7 +58,7 @@ class Signup extends Component {
       <div className="signin" style={{ color: 'black' }}>
         <img src="./assets/logo.png" alt="logo" />
         <h1>Elearning</h1>
-        <h3>Apprendre en s’amusant de façon ludique !</h3>
+        <h3>Apprendre en s’amusant de façon ludique</h3>
         {
           connected
             ? <p>Signed in!</p>

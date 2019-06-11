9d8263bf5df3cf9d12de3050bdbde3e137d02f21
David <belmontecast@gmail.com>
2019-06-11 09:52:55 +0200

    /n enlevé

--- src/components/SlideApprenant.js
+++ src/components/SlideApprenant.js
@@ -1,50 +1,56 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router';
+import ReactHtmlParser from 'react-html-parser';
 import withFirebaseContext from '../Firebase/withFirebaseContext';
 import '../App.scss';
-import ReactHtmlParser from 'react-html-parser'; 
+
+// const regex = /\n/gm;
+// const subst = '<br />';
+
 
 class SlideApprenant extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            infoSlide: null,
-        };
-        this.getInfo();
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      infoSlide: { slides: [] },
+    };
+    this.getInfo();
+  }
 
-    getInfo = () => {
-        const { firestore } = this.props;
-        const docRef = firestore.collection('parcours').doc(localStorage.getItem('id')).collection('cours').doc(localStorage.getItem('id'));
-        docRef.get().then((doc) => {
-            if (doc.exists) {
-                const infoSlide = doc.data();
-                this.setState({
-                    infoSlide,
-                });
-            } else {
-                // doc.data() will be undefined in this case
-                console.log('No such document!');
-            }
-        }).catch((error) => {
-            console.log('Error getting document:', error);
+  getInfo = () => {
+    const { firestore } = this.props;
+    const docRef = firestore.collection('parcours').doc(localStorage.getItem('id')).collection('cours').doc(localStorage.getItem('id'));
+    docRef.get().then((doc) => {
+      if (doc.exists) {
+        const infoSlide = doc.data();
+        this.setState({
+          infoSlide,
         });
-    }
+      } else {
+        // doc.data() will be undefined in this case
+        console.log('No such document!');
+      }
+    }).catch((error) => {
+      console.log('Error getting document:', error);
+    });
+  }
 
-
-    render() {
-        const { infoSlide } = this.state;
-        return (
-            <>
-                <div style={{ border: '2px solid black' }}>
-                    {infoSlide && ReactHtmlParser (infoSlide.slides[1]) }
-                </div>
-                <div style={{ border: '2px solid black' }}>
-                {infoSlide && ReactHtmlParser (infoSlide.slides[2]) }
-                </div>
-            </>
-        );
-    }
+  render() {
+    const { infoSlide } = this.state;
+    return (
+      <>
+        {
+          infoSlide.slides && Object.values(infoSlide.slides).map(sl => <div style={{ color: 'white' }}>{ReactHtmlParser(sl)}</div>)
+        }
+        {/* <pre style={{ border: '2px solid black' }}>
+            {infoSlide && ReactHtmlParser(infoSlide.slides[1].replace(regex, subst)) }
+          </pre>
+          <pre style={{ border: '2px solid black' }}>
+            {infoSlide && ReactHtmlParser(infoSlide.slides[2].replace(regex, subst)) }
+          </pre> */}
+      </>
+    );
+  }
 }
 
 export default withRouter(withFirebaseContext(SlideApprenant));

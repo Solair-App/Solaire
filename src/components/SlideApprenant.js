c84e697041d956156fb1ed04358a123b775b6ce4
David <belmontecast@gmail.com>
2019-06-13 11:56:31 +0200

    slideApprenant hooks

--- src/components/SlideApprenant.js
+++ src/components/SlideApprenant.js
@@ -1,27 +1,16 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { withRouter } from 'react-router';
 import ReactHtmlParser from 'react-html-parser';
 import withFirebaseContext from '../Firebase/withFirebaseContext';
 import '../App.scss';
 
-class SlideApprenant extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      infoSlide: { slides: [] },
-    };
-    this.getInfo();
-  }
-
-  getInfo = () => {
-    const { firestore } = this.props;
+const SlideApprenant = ({ firestore }) => {
+  const [infoSlide, setSlide] = useState({ slides: [] });
+  useEffect(() => {
     const docRef = firestore.collection('parcours').doc(localStorage.getItem('id')).collection('cours').doc(localStorage.getItem('coursId'));
     docRef.get().then((doc) => {
       if (doc.exists) {
-        const infoSlide = doc.data();
-        this.setState({
-          infoSlide,
-        });
+        setSlide(doc.data());
       } else {
         // doc.data() will be undefined in this case
         console.log('No such document!');
@@ -29,24 +18,16 @@ class SlideApprenant extends Component {
     }).catch((error) => {
       console.log('Error getting document:', error);
     });
-  }
+  });
 
-  render() {
-    const { infoSlide } = this.state;
-    return (
-      <>
-        {
-          infoSlide.slides && Object.values(infoSlide.slides).map(sl => <div style={{ color: 'black' }}>{ReactHtmlParser(sl)}</div>)
-        }
-        {/* <pre style={{ border: '2px solid black' }}>
-            {infoSlide && ReactHtmlParser(infoSlide.slides[1].replace(regex, subst)) }
-          </pre>
-          <pre style={{ border: '2px solid black' }}>
-            {infoSlide && ReactHtmlParser(infoSlide.slides[2].replace(regex, subst)) }
-          </pre> */}
-      </>
-    );
-  }
-}
+  return (
+    <div>
+      {
+        infoSlide.slides && Object.values(infoSlide.slides).map(sl => <div style={{ color: 'black' }}>{ReactHtmlParser(sl)}</div>)
+      }
+    </div>
+  );
+};
+
 
 export default withRouter(withFirebaseContext(SlideApprenant));

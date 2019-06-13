c04ff8bae426c6a298c4314576c029d89da036eb
David <belmontecast@gmail.com>
2019-06-13 16:25:11 +0200

    Slide Apprenant Responsive

--- src/components/SlideApprenant.js
+++ src/components/SlideApprenant.js
@@ -1,8 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import { withRouter } from 'react-router';
 import ReactHtmlParser from 'react-html-parser';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import MobileStepper from '@material-ui/core/MobileStepper';
+import Button from '@material-ui/core/Button';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
 import withFirebaseContext from '../Firebase/withFirebaseContext';
 import '../App.scss';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    maxWidth: 400,
+    flexGrow: 1,
+  },
+  header: {
+    display: 'flex',
+    alignItems: 'center',
+    height: 50,
+    paddingLeft: theme.spacing(4),
+    backgroundColor: theme.palette.background.default,
+  },
+
+}));
 
 const SlideApprenant = ({ firestore }) => {
   const [infoSlide, setSlide] = useState({ slides: [] });
@@ -19,15 +39,47 @@ const SlideApprenant = ({ firestore }) => {
       console.log('Error getting document:', error);
     });
   });
+  const classes = useStyles();
+  const theme = useTheme();
+  const [activeStep, setActiveStep] = React.useState(0);
+  const maxSteps = infoSlide.slides && Object.values(infoSlide.slides).length;
+
+
+  function handleNext() {
+    setActiveStep(prevActiveStep => prevActiveStep + 1);
+  }
+
+  function handleBack() {
+    setActiveStep(prevActiveStep => prevActiveStep - 1);
+  }
 
   return (
-    <div>
+    <div className={classes.root}>
+
       {
-        infoSlide.slides && Object.values(infoSlide.slides).map(sl => <div style={{ color: 'black' }}>{ReactHtmlParser(sl)}</div>)
+        infoSlide.slides && Object.values(infoSlide.slides).map(sl => <div className="import">{ReactHtmlParser(sl)[activeStep]}</div>)
       }
+
+      <MobileStepper
+        steps={maxSteps}
+        position="static"
+        variant="text"
+        activeStep={activeStep}
+        nextButton={(
+          <Button size="small" onClick={handleNext} disabled={activeStep === maxSteps - 1}>
+            Next
+            {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+          </Button>
+)}
+        backButton={(
+          <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+            {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+            Back
+          </Button>
+)}
+      />
     </div>
   );
 };
-
 
 export default withRouter(withFirebaseContext(SlideApprenant));

e4b43fc65609ba611e8883ea179af20a49b5f5f7
David <belmontecast@gmail.com>
2019-06-14 10:03:12 +0200

    MAJ CreateSlider

--- src/components/CreateSlider.js
+++ src/components/CreateSlider.js
@@ -89,7 +89,7 @@ const CreateSlider = ({ firestore, history }) => {
       }
 
       {
-        infoSlide.slides && Object.values(infoSlide.slides).map(sl => <div className="import">{ReactHtmlParser(sl)[activeStep]}</div>)
+        <div className="import">{ReactHtmlParser(infoSlide.slides && Object.values(infoSlide.slides)[activeStep])}</div>
       }
 
       <MobileStepper

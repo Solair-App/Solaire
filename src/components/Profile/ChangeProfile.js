cb386fdd9fb0e001547f9c54bf543cb599d821d9
Justine Vareille <vareille.justine@gmail.com>
2019-06-17 11:09:40 +0200

    pourquoi

--- src/components/Profile/ChangeProfile.js
+++ src/components/Profile/ChangeProfile.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import ArrowBack from '@material-ui/icons/ArrowBack';
+import TextField from '@material-ui/core/TextField';
 
 class ChangeProfile extends Component {
   constructor(props) {
@@ -8,7 +11,64 @@ class ChangeProfile extends Component {
 
   render() {
     return (
-      <div />
+      <div>
+        <ArrowBack
+          style={{ position: 'fixed', left: '10px', top: '10px' }}
+          onClick={() => {
+            this.redirect('/Profile');
+          }}
+        />
+        <div>
+          <TextField
+            required
+            id="standard-name"
+            label="Nom"
+            className="textfield"
+            style={{ marginTop: '5%', width: '50%' }}
+          />
+        </div>
+
+        <div>
+          <TextField
+            required
+            id="standard-name"
+            label="Prénom"
+            className="textfield"
+            style={{ marginTop: '5%', width: '50%' }}
+          />
+        </div>
+
+        <div>
+          <TextField
+            required
+            id="outlined-email-input"
+            label="Email"
+            className="textField"
+            autoComplete="email"
+            style={{ marginTop: '5%', width: '50%' }}
+          />
+        </div>
+
+        <div>
+          <TextField
+            id="standard-name"
+            label="Ville"
+            className="textfield"
+            style={{ marginTop: '5%', width: '50%' }}
+          />
+        </div>
+
+        <div>
+          <TextField
+            id="standard-multiline-flexible"
+            label="Description"
+            multiline
+            rowsMax="4"
+            className="textField"
+            style={{ marginTop: '5%', width: '50%' }}
+          />
+        </div>
+      </div>
 
     );
   }

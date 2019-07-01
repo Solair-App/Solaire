776391813ca892fa824d0248f3f95bd1c7c7dd84
Lau <laurent.henriques@gmail.com>
2019-07-01 11:24:17 +0200

    change rating place

--- src/components/dashboard/List.js
+++ src/components/dashboard/List.js
@@ -1,33 +1,33 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import Rating from 'material-ui-rating';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import './List.css';
+import Rating from "material-ui-rating";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import "./List.css";
 
-import Typography from '@material-ui/core/Typography';
+import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles({
   card: {
     width: 150,
-    height: 200,
+    height: 200
   },
   title: {
     fontSize: 11,
-    fontWeight: 'bold',
+    fontWeight: "bold"
   },
   media: {
-    height: 80,
+    height: 80
   },
   pos: {
-    fontSize: 10,
-  },
+    fontSize: 10
+  }
 });
 
 export default function ListCours(props) {
@@ -36,75 +36,82 @@ export default function ListCours(props) {
 
   return (
     <ul className="hs full">
-      {data && data.filter(parcours => parcours.data.tags.toUpperCase().includes(searchField.toUpperCase()))
-       
-        .map((info, index) => (
-          
-          <li className="item" key={`${index + 1}n`}><Rating readOnly value={info.data.rating} />
-            <Card className={classes.card}>
-              <CardActionArea>
-                <Link to={`/parcours/${info.id}`} className="link">
-                  <CardMedia
-                    className={classes.media}
-                    image="https://image.noelshack.com/fichiers/2019/26/2/1561453439-992481.png"
-                    title="Contemplative Reptile"
-                  />
-                </Link>
-                <CardContent style={{ marginBottom: -15 }}>
+      {data &&
+        data
+          .filter(parcours =>
+            parcours.data.tags.toUpperCase().includes(searchField.toUpperCase())
+          )
+
+          .map((info, index) => (
+            <li className="item" key={`${index + 1}n`}>
+            
+              <Card className={classes.card}>
+                <CardActionArea>
                   <Link to={`/parcours/${info.id}`} className="link">
-                    <div className="boxTitle">
+                    <CardMedia
+                      className={classes.media}
+                      image="https://image.noelshack.com/fichiers/2019/26/2/1561453439-992481.png"
+                      title="Contemplative Reptile"
+                    />
+                  </Link>
+                  <CardContent style={{ marginBottom: -15 }}>
+                    <Link to={`/parcours/${info.id}`} className="link">
+                      <div className="boxTitle">
+                        <Typography
+                          gutterBottom
+                          variant="h5"
+                          component="h2"
+                          className={classes.title}
+                          color="textSecondary"
+                        >
+                          <image />
+                          {info.data.name}
+                        </Typography>
+                      </div>
                       <Typography
-                        gutterBottom
-                        variant="h5"
-                        component="h2"
-                        className={classes.title}
+                        variant="body2"
+                        component="p"
+                        className={classes.pos}
                         color="textSecondary"
                       >
-                        <image />
-                        {info.data.name}
+                        {info.data.difficulté}
                       </Typography>
-                    </div>
-                    <Typography
-                      variant="body2"
-                      component="p"
-                      className={classes.pos}
-                      color="textSecondary"
-                    >
-                      {info.data.difficulté}
-                    </Typography>
-                    <Typography
-                      variant="body2"
-                      component="p"
-                      className={classes.pos}
-                      color="textSecondary"
-                    >
-                      {info.data.langue}
-                    </Typography>
-                    <Typography
-                      variant="body2"
-                      component="p"
-                      className={classes.pos}
-                      color="textSecondary"
-                    >
-                      {info.data.durée}
-                    </Typography>
-                  </Link>
-                </CardContent>
-              </CardActionArea>
-              <CardActions>
-                <Button
-                  size="small"
-                  color="primary"
-                  style={{
-                    marginTop: -8, marginBottom: -5, fontSize: 11, marginLeft: -4,
-                  }}
-                >
+                      <Typography
+                        variant="body2"
+                        component="p"
+                        className={classes.pos}
+                        color="textSecondary"
+                      >
+                        {info.data.langue}
+                      </Typography>
+                      <Typography
+                        variant="body2"
+                        component="p"
+                        className={classes.pos}
+                        color="textSecondary"
+                      >
+                        {info.data.durée}
+                      </Typography>
+                    </Link>
+                  </CardContent>  <Rating readOnly value={info.data.rating} />
+                </CardActionArea>
+                <CardActions>
+                  <Button
+                    size="small"
+                    color="primary"
+                    style={{
+                      marginTop: -8,
+                      marginBottom: -5,
+                      fontSize: 11,
+                      marginLeft: -4
+                    }}
+                  >
                     Share
-                </Button>
-              </CardActions>
-            </Card>
-          </li>
-        ))}
+                  </Button>  
+                </CardActions>
+              </Card>
+            </li>
+          ))}
     </ul>
   );
 }
